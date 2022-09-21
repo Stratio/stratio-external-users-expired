82a3e8f415e31afc0db3821be80dc0a8b5405b8e
hbermudez <hbermudez@stratio.com>
2022-09-21 16:12:35 +0200

    Update main to delete users

--- main.py
+++ main.py
@@ -50,12 +50,31 @@ def get_ldap_users(config, days_expire):
 
     users = []
     managers = []
+    users_delete = []
+    managers_delete = []
     now = calendar.timegm(time.gmtime())
     epoch_expire = days_expire * 24 * 60 * 60
     for user in conn.entries:
         service = ""
         try:
-            if int(str(user.shadowExpire)) - now < epoch_expire:
+            if int(str(user.shadowExpire)) < now:
+                for description in user.description:
+
+                    if "Responsable" not in description:
+                        service = str(description)
+
+                    if "Responsable" in description:
+                        manager = str(description).split(',')[0].split(': ')[1].strip()
+                        if manager not in managers:
+                            managers_delete.append(manager)
+                        users_delete.append({"user": str(user.uid),
+                                             "mail": str(user.mail),
+                                             "expire": str(user.shadowExpire),
+                                             "manager": str(manager),
+                                             "service": str(service),
+                                             "jira": str(description).split(',')[1].split(': ')[1].strip()})
+
+            elif int(str(user.shadowExpire)) - now < epoch_expire:
                 for description in user.description:
 
                     if "Responsable" not in description:
@@ -75,19 +94,128 @@ def get_ldap_users(config, days_expire):
         except LDAPCursorError:
             logging.error("Cant get LDAP Users")
             exit(-1)
-    return managers, users
+    return managers, users, managers_delete, users_delete
 
 
-def send_mails(config, managers, users, days):
+def delete_ldap_users(config, users, dry_run):
+    if not dry_run:
+        logging.info('Connecting to LDAP server {}'.format(config['LDAP']['server'].split(':')[0]))
+        logging.info('Connecting to LDAP port {}'.format(config['LDAP']['server'].split(':')[1]))
+
+        server = Server(config['LDAP']['server'].split(':')[0],
+                        port=int(config['LDAP']['server'].split(':')[1]),
+                        get_info=ALL)
+
+        logging.info('Connecting to LDAP with username {}'.format(config['LDAP']['user_name']))
+        logging.info('Connecting to LDAP with password ****')
+        conn = Connection(server,
+                          user=config['LDAP']['user_name'],
+                          password=config['LDAP']['password'],
+                          authentication=SIMPLE,
+                          auto_bind="NO_TLS")
+        for user in users:
+            logging.info('Delete user in LDAP: {}'.format(user['user']))
+
+            conn.search(config['LDAP']['base_search'],
+                        "(uid={})".format(user["user"]),
+                        attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
+
+            user_dn = str(conn.entries[0]).partition('\n')[0].split(" - ")[0].split(": ")[1]
+            logging.info('Delete user in LDAP with DN: {}'.format(user_dn))
+            conn.delete(user_dn)
+    else:
+        logging.info("Param --dry-run detected, no user was deleted")
+
+
+def send_mail_delete(config, managers, users, dry_run):
+    logging.info("Prepare to delete old users")
+    for manager in managers:
+        mail_headers = """From: <sistemas@stratio.com>
+To: To Person <{}@stratio.com>
+MIME-Version: 1.0
+Content-type: text/html
+Subject: Remove external user access
+""".format(manager)
+        
+        mail_body = """
+Hi: 
+<br><br>
+Next access users were removed from their services associated:
+<br>
+<table>
+  <tr>
+    <th>User</th>
+    <th>Mail</th>
+    <th>Expire Date</th>
+    <th>Service</th>
+    <th>Jira</th>
+  </tr>
+"""
+        mail_users = ""
+        for user in users:
+            if user["manager"] == manager:
+                mail_users = mail_users + """  <tr>
+    <td>{}</td>
+    <td>{}</td>
+    <td>{}</td>
+    <td>{}</td>
+""".format(user["user"],
+           user["mail"],
+           time.strftime('%d-%m-%Y', time.localtime(int(user["expire"]))),
+           user["service"])
+
+                if "??" in user["jira"]:
+                    mail_users = mail_users +"""    <td>???</td>
+  </tr>
+"""
+                else:
+                    mail_users = mail_users + """    <td>https://stratio.atlassian.net/browse/{}</td>
+  </tr>
+""".format(user["jira"])
+
+        mail_tail = """</table>
+<br>
+You must notify the <b>team responsible</b> for the service so that the user does not lose access <b>before its expiration date</b>.
+<br><br>
+Thanks for your time
+"""
+        mail_text = mail_headers + mail_body + mail_users + mail_tail
+
+        logging.debug("Sending mail:")
+        logging.debug('\t\t'.join(mail_text.splitlines(True)))
+        if not dry_run:
+            try:
+                smtp_obj = smtplib.SMTP(str(config['MAIL']['host']),
+                                        int(str(config['MAIL']['port'])))
+                smtp_obj.connect(str(config['MAIL']['host']),
+                                 int(str(config['MAIL']['port'])))
+                smtp_obj.ehlo()
+                smtp_obj.starttls()
+                smtp_obj.ehlo()
+                smtp_obj.login(user=str(config['MAIL']['user']),
+                               password=str(config['MAIL']['password']))
+
+                smtp_obj.sendmail("sistemas@stratio.com",
+                                  "{}@stratio.com".format(manager),
+                                  mail_text)
+                smtp_obj.quit()
+
+            except smtplib.SMTPException as e:
+                logging.error("Unable to send email to {}: {}".format(manager, e))
+        else:
+            logging.info("Param --dry-run detected, mail didn't send")
+
+
+def send_mails(config, managers, users, days, dry_run):
     logging.info("Prepare to send emails to managers")
     for manager in managers:
         logging.debug("Sending mail to {}@stratio.com".format(manager))
         mail_headers = """From: <sistemas@stratio.com>
-To: To Person <hbermudez@stratio.com>
+To: To Person <{}@stratio.com>
 MIME-Version: 1.0
 Content-type: text/html
 Subject: External user access expiration
-"""
+""".format(manager)
 
         mail_body = """
 Hi: 
@@ -111,13 +239,19 @@ This is friendly reminder: <b>next users will lose their access in the next {} d
     <td>{}</td>
     <td>{}</td>
     <td>{}</td>
-    <td>https://stratio.atlassian.net/browse/{}</td>
-  </tr>
 """.format(user["user"],
            user["mail"],
-           time.strftime('%Y-%m-%d', time.localtime(int(user["expire"]))),
-           user["service"],
-           user["jira"])
+           time.strftime('%d-%m-%Y', time.localtime(int(user["expire"]))),
+           user["service"])
+
+                if "??" in user["jira"]:
+                    mail_users = mail_users +"""    <td>???</td>
+  </tr>
+"""
+                else:
+                    mail_users = mail_users + """    <td>https://stratio.atlassian.net/browse/{}</td>
+  </tr>
+""".format(user["jira"])
 
         mail_tail = """</table>
 <br>
@@ -129,28 +263,27 @@ Thanks for your time
 
         logging.debug("Sending mail:")
         logging.debug('\t\t'.join(mail_text.splitlines(True)))
-        try:
-            smtp_obj = smtplib.SMTP(str(config['MAIL']['host']),
-                                    int(str(config['MAIL']['port'])))
-            smtp_obj.connect(str(config['MAIL']['host']),
-                             int(str(config['MAIL']['port'])))
-            smtp_obj.ehlo()
-            smtp_obj.starttls()
-            smtp_obj.ehlo()
-            smtp_obj.login(user=str(config['MAIL']['user']),
-                           password=str(config['MAIL']['password']))
+        if not dry_run:
+            try:
+                smtp_obj = smtplib.SMTP(str(config['MAIL']['host']),
+                                        int(str(config['MAIL']['port'])))
+                smtp_obj.connect(str(config['MAIL']['host']),
+                                 int(str(config['MAIL']['port'])))
+                smtp_obj.ehlo()
+                smtp_obj.starttls()
+                smtp_obj.ehlo()
+                smtp_obj.login(user=str(config['MAIL']['user']),
+                               password=str(config['MAIL']['password']))
 
-            # smtp_obj.sendmail("sistemas@stratio.com",
-            #                   "{}@stratio.com".format(manager),
-            #                   mail)
-            smtp_obj.sendmail("sistemas@stratio.com",
-                              "hbermudez@stratio.com",
-                              mail_text)
-            smtp_obj.quit()
+                smtp_obj.sendmail("sistemas@stratio.com",
+                                  "{}@stratio.com".format(manager),
+                                  mail_text)
+                smtp_obj.quit()
 
-        except smtplib.SMTPException as e:
-            logging.error("Unable to send email to {}: {}".format(manager, e))
-        exit(0)
+            except smtplib.SMTPException as e:
+                logging.error("Unable to send email to {}: {}".format(manager, e))
+        else:
+            logging.info("Param --dry-run detected, mail didn't send")
 
 
 def main():
@@ -162,6 +295,16 @@ def main():
                         dest="config_file_path",
                         default=CONFIG_FILE_PATH,
                         help="Path to the config file in .ini format. Default: config.ini")
+    parser.add_argument("-d", "--delete",
+                        dest="delete",
+                        action="store_true",
+                        default=False,
+                        help="Delete expired users instead of near expired")
+    parser.add_argument("--dry-run",
+                        dest="dry_run",
+                        action="store_true",
+                        default=False,
+                        help="Dont send email either delete users, only print log")
     parser.add_argument("-l", "--log",
                         dest="log_level",
                         default=DEFAULT_LOGGING,
@@ -185,8 +328,14 @@ def main():
         logging.error("Days must be a int > 0")
         exit(-1)
     config = load_config(args.config_file_path)
-    managers, users = get_ldap_users(config, args.days)
-    send_mails(config, managers, users, args.days)
+    managers, users, managers_delete, users_delete = get_ldap_users(config, args.days)
+    if args.delete:
+        send_mail_delete(config, managers_delete, users_delete, args.dry_run)
+        delete_ldap_users(config, users_delete, args.dry_run)
+    else:
+        send_mails(config, managers, users, args.days, args.dry_run)
+
+    exit(0)
 
 
 # MAIN.
